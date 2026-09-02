4528b16bd786173e68d358f1461efcd3ce6d4fce
Talal Azhar <talalazhar5656@gmail.com>
2026-09-02 07:55:51 +0500

    hashing in python

---
+++ Hashing/hasing.py
@@ -0,0 +1,5 @@
+import hashlib
+message = input("Enter Your Message :")
+hashedValue = hashlib.sha256(message.encode()).hexdigest()
+print("Message :", message)
+print("Hashed Value :", hashedValue)
